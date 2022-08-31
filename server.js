b0c5af56a8269b47d2799acd8cbcfb5e63ae088a
zehang <houzehang@douyuxingchen.com>
2022-08-31 16:01:22 +0800

    bugfix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1')
   res.header("Content-Type", "application/json;charset=utf-8");
-  console.log('DEBUG_LOG:all', req);
+  console.log('DEBUG_LOG:all', req.body);
   next();
 });
 
