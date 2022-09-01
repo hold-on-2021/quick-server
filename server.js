f8f6277a797a6624b4cb34f7cce1ac33cc675572
zehang <houzehang@douyuxingchen.com>
2022-09-01 17:24:22 +0800

    bugfix

--- server.js
+++ server.js
@@ -17,52 +17,52 @@ app.all('*', function (req, res, next) {
 	if (req.body && req.body.url) {
 		let detail = req.body;
 
-		axios.post('https://open.feishu.cn/open-apis/bot/v2/hook/bff72dd1-95ac-43a6-b764-bf809fd9f682', {
-			"msg_type": "post",
-			"content": {
-				"post": {
-					"zh_cn": {
-						"title": "程序异常通知",
-						"content": [
-							[{
-								"tag": "text",
-								"text": `项目: ${detail.project_name}`
-							}],
-							[{
-								"tag": "text",
-								"text": `问题描述: ${detail.message}`
-							}],
-							[{
-								"tag": "text",
-								"text": `问题等级: ${detail.level}`
-							}],
-							[
-								detail.event && {
+		// axios.post('https://open.feishu.cn/open-apis/bot/v2/hook/bff72dd1-95ac-43a6-b764-bf809fd9f682', {
+		// 	"msg_type": "post",
+		// 	"content": {
+		// 		"post": {
+		// 			"zh_cn": {
+		// 				"title": "程序异常通知",
+		// 				"content": [
+		// 					[{
+		// 						"tag": "text",
+		// 						"text": `项目: ${detail.project_name}`
+		// 					}],
+		// 					[{
+		// 						"tag": "text",
+		// 						"text": `问题描述: ${detail.message}`
+		// 					}],
+		// 					[{
+		// 						"tag": "text",
+		// 						"text": `问题等级: ${detail.level}`
+		// 					}],
+		// 					[
+		// 						detail.event && {
 
-									"tag": "text",
-									"text": `发生时间戳: ${detail.event.timestamp}`
-								}
-							],
-							[
-								detail.event && {
-									"tag": "text",
-									"text": `运行环境: ${detail.event.environment}`
-								}
-							],
-							[{
-								"tag": "text",
-								"text": `Sentry登陆帐号｜密码: ${'992340655@qq.com｜123456'}`
-							}],
-							[{
-								"tag": "a",
-								"text": "前往查看",
-								"href": detail.url
-							}]
-						]
-					}
-				}
-			}
-		})
+		// 							"tag": "text",
+		// 							"text": `发生时间戳: ${detail.event.timestamp}`
+		// 						}
+		// 					],
+		// 					[
+		// 						detail.event && {
+		// 							"tag": "text",
+		// 							"text": `运行环境: ${detail.event.environment}`
+		// 						}
+		// 					],
+		// 					[{
+		// 						"tag": "text",
+		// 						"text": `Sentry登陆帐号｜密码: ${'992340655@qq.com｜123456'}`
+		// 					}],
+		// 					[{
+		// 						"tag": "a",
+		// 						"text": "前往查看",
+		// 						"href": detail.url
+		// 					}]
+		// 				]
+		// 			}
+		// 		}
+		// 	}
+		// })
 	}
 	next();
 });
